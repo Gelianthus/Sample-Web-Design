f1a809ef9393d0982e01b2bb823adfaa82b66dcf
Gelianthus <109640026+Gelianthus@users.noreply.github.com>
2022-10-12 10:39:04 +0800

    added dropdown

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const headerElem = document.querySelector("header");
 const mainElem = document.querySelector("main");
 
 mainElem.style.paddingTop = headerElem.clientHeight + "px";
+
+const exploreProjectButton = document.getElementById("exploreProjectButton");
+const projectUL = document.getElementById("projectUL");
+
+exploreProjectButton.addEventListener("click", function(){
+    if(projectUL.classList.contains("visibility-display")) {
+        projectUL.classList.remove("visibility-display");
+    } else {
+        projectUL.classList.add("visibility-display");
+    }
+})
+
+projectUL.addEventListener("mouseleave", function() {
+    projectUL.classList.add("visibility-display");
+})
