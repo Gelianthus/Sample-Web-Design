01604b34aa7a579722e5c4d87cdd0eb92b0b02bc
Gelianthus <109640026+Gelianthus@users.noreply.github.com>
2022-10-14 02:48:45 +0800

    Added commission section

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ const poems = document.getElementById("poems");
 const articles = document.getElementById("articles");
 const shortStories = document.getElementById("short-stories");
 
+const aboutUs = document.getElementById("aboutUs");
+const placeCommission = document.getElementById("placeCommission");
+
 // Scripts 
 
 poems.style.scrollMarginTop = headerElem.clientHeight + "px";
 articles.style.scrollMarginTop = headerElem.clientHeight + "px";
 shortStories.style.scrollMarginTop = headerElem.clientHeight + "px";
+
+aboutUs.style.scrollMarginTop = headerElem.clientHeight + "px";
+placeCommission.style.scrollMarginTop = headerElem.clientHeight + "px";
 
 mainElem.style.paddingTop = headerElem.clientHeight + "px";
 
