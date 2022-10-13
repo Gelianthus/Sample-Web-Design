26cbe9b1a2b087672577cecd878b648e8601203f
Gelianthus <109640026+Gelianthus@users.noreply.github.com>
2022-10-13 14:02:19 +0800

    added project section

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+// HTML Element Declarations
+
 const headerElem = document.querySelector("header");
 const mainElem = document.querySelector("main");
 
-mainElem.style.paddingTop = headerElem.clientHeight + "px";
-
 const exploreProjectButton = document.getElementById("exploreProjectButton");
 const projectUL = document.getElementById("projectUL");
+
+const poems = document.getElementById("poems");
+const articles = document.getElementById("articles");
+const shortStories = document.getElementById("short-stories");
+
+// Scripts 
+
+poems.style.scrollMarginTop = headerElem.clientHeight + "px";
+articles.style.scrollMarginTop = headerElem.clientHeight + "px";
+shortStories.style.scrollMarginTop = headerElem.clientHeight + "px";
+
+mainElem.style.paddingTop = headerElem.clientHeight + "px";
 
 exploreProjectButton.addEventListener("click", function(){
     if(projectUL.classList.contains("visibility-display")) {
